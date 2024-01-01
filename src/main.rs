0586f704bcec6c41c77b6a6cd297b24f4616577f
Anton Whalley <anton@venshare.com>
2024-01-01 02:28:15 +0000

    fix: updated panic libs
    Signed-off-by: Anton Whalley <anton@venshare.com>

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,10 @@
 
 // dev profile: easier to debug panics; can put a breakpoint on `rust_begin_unwind`
 #[cfg(debug_assertions)]
-use panic_halt as _;
+use panic_semihosting as _;
 
-// release profile: minimize the binary size of the application
 #[cfg(not(debug_assertions))]
-use panic_abort as _;
+use panic_halt as _;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
