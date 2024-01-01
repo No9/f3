b3c2e62478e7e42e6a0420258ae4e9af31d0c417
Anton Whalley <anton@venshare.com>
2024-01-01 02:08:28 +0000

    add: panic deps
    Signed-off-by: Anton Whalley <anton@venshare.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #![no_main]
 #![no_std]
 
+// dev profile: easier to debug panics; can put a breakpoint on `rust_begin_unwind`
+#[cfg(debug_assertions)]
 use panic_halt as _;
+
+// release profile: minimize the binary size of the application
+#[cfg(not(debug_assertions))]
+use panic_abort as _;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
